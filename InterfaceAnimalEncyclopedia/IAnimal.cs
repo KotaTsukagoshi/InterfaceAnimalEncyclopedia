fdcc2c4231d5fe70ef831567ab1fe3d102303d6f
MitsuruKanazawa <uni202406_07@telework.to>
2024-08-19 13:57:07 +0900

    継承追加

--- InterfaceAnimalEncyclopedia/IAnimal.cs
+++ InterfaceAnimalEncyclopedia/IAnimal.cs
@@ -11,4 +11,28 @@ namespace InterfaceAnimalEncyclopedia
         string MakeSound();
     }
 
+    public class Dog : IAnimal
+    {
+        public string MakeSound()
+        {
+            return "ワンワン";
+        }
+    }
+
+    public class Cat : IAnimal
+    {
+        public string MakeSound()
+        {
+            return "ニャー";
+        }
+    }
+
+    public class Bird : IAnimal
+    {
+        public string MakeSound()
+        {
+            return "ピーピー";
+        }
+    }
+
 }
