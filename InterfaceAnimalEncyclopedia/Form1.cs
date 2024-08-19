0f6ada69d77085b60bd0caae52b9bb16f1e92378
TakekiOkiyama <uni202406_04@telework.to>
2024-08-19 14:04:20 +0900

    F尾rmの変更

--- InterfaceAnimalEncyclopedia/Form1.cs
+++ InterfaceAnimalEncyclopedia/Form1.cs
@@ -12,10 +12,43 @@ namespace InterfaceAnimalEncyclopedia
 {
     public partial class Form1 : Form
     {
+        Dictionary<string, string> AnimalNames;
         public Form1()
         {
             InitializeComponent();
-            //
+            this.AnimalNames = new Dictionary<string, string>();
+            this.AnimalNames.Add("", "2");
+            this.AnimalNames.Add("犬", "3");
+            this.AnimalNames.Add("猫", "1");
+            this.AnimalNames.Add("鳥", "2");
+            foreach (KeyValuePair<string, string> data in this.AnimalNames)
+            {
+                comboBox1.Items.Add(data.Key);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IAnimal selectedAnimal = null;
+          
+            switch (comboBox1.SelectedItem.ToString())
+            {
+                case "犬":
+                    selectedAnimal = new Dog();
+                    break;
+                case "猫":
+                    selectedAnimal = new Cat();
+                    break;
+                case "鳥":
+                    selectedAnimal = new Bird();
+                    break;
+                default:
+                    MessageBox.Show("動物を選んでください。");
+                    return;
+            }
+
+            label1.Text = selectedAnimal.MakeSound();
         }
     }
+    
 }
